0cc6d43d198135e9741f51b26e7b4002b31bf738
kit <venkata.amulya@gmail.com>
2017-08-09 16:47:29 -0500

    Initial commit

---
+++ public/javascripts/find_db.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var mongodb = require('mongodb');
+var mongoClient = mongodb.MongoClient;
+var app = express();
+var url = 'mongodb://localhost:27017/jdproject';
+mongoClient.connect(url, function(err, db) {
+    createDocuments(db, function() {
+        db.close();
+    });
+});
+var createDocuments  = function(db, callback) {
+    var collection = db.collection("jduser");
+    collection.insert([
+        {firstname : "amulya",lastname: "komatineni",emailid: "amulya@gmail.com"},
+        {firstname : "sravani",lastname: "komatineni",emailid: "sravani@gmail.com"},
+        {firstname : "hemalatha",lastname: "komatineni",emailid: "hema@gmail.com"},
+    ], function(err, result) {
+        callback(result);
+    });
+    var server= app.listen(9000, function(){
+        console.log('server running on port 9000');
+    });
+    app.get('/', function(req, res) {
+        res.send('welcome to mongodb application');
+    });
+}
